6c46edca1629ee64b8b134eaca100da46ebf9615
Yoshida Kanae <xiaoxi654@outlook.com>
2022-08-09 02:23:58 +0800

    Some font tweak for Steam

--- desktop/software/fonts.nix
+++ desktop/software/fonts.nix
@@ -2,21 +2,27 @@
 
 {
   fonts = {
-    enableDefaultFonts = false;
     fonts = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       noto-fonts-extra
       noto-fonts-emoji
+      sarasa-gothic
+      wqy_zenhei
       jetbrains-mono
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
-    fontconfig.defaultFonts = pkgs.lib.mkForce {
-      serif = [ "Noto Serif" "Noto Serif CJK SC" ];
-      sansSerif = [ "Noto Sans" "Noto Sans CJK SC" ];
-      monospace = [ "Jetbrains Mono" ];
-      emoji = [ "Noto Color Emoji" ];
+    fontconfig = {
+      enable = true;
+      hinting = {
+        enable = true;
+      };
+      defaultFonts = {
+        serif = [ "Noto Serif CJK SC" ];
+        sansSerif = [ "Sarasa UI SC" "Noto Sans CJK SC" ];
+        monospace = [ "Jetbrains Mono" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
     };
   };
 }
