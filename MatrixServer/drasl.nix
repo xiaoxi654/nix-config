92e8d9285e23717d753a74249c57254cf37d21ee
Yoshida Kanae <i@izm.im>
2026-08-03 03:08:21 +0800

    Some cleanup

--- MatrixServer/drasl.nix
+++ MatrixServer/drasl.nix
@@ -4,14 +4,14 @@ let
   draslToml = lib.importTOML ./configs/drasl.toml;
 in
 {
-    sops.secrets.drasl_oidc_client_secret = { };
+  sops.secrets.drasl_oidc_client_secret = { };
 
-    services.drasl = {
-      enable = true;
-      settings = draslToml // {
-        RegistrationOIDC = map (provider: provider // {
-          ClientSecretFile = config.sops.secrets.drasl_oidc_client_secret.path;
-        }) draslToml.RegistrationOIDC;
-      };
+  services.drasl = {
+    enable = true;
+    settings = draslToml // {
+      RegistrationOIDC = map (provider: provider // {
+        ClientSecretFile = config.sops.secrets.drasl_oidc_client_secret.path;
+      }) draslToml.RegistrationOIDC;
     };
+  };
 }
