02f3f24acf68b4a1a56fe8147b9e98456f2af1cc
Yoshida Kanae <i@izm.im>
2026-08-13 01:06:45 +0800

    Add Kioubit Auth, update flake.lock

--- MatrixServer/drasl.nix
+++ MatrixServer/drasl.nix
@@ -2,15 +2,22 @@
 
 let
   draslToml = lib.importTOML ./configs/drasl.toml;
+
+  # Maps each OIDC provider name to its sops-encrypted client secret.
+  oidcClientSecretNames = {
+    "oauth.dn42" = "drasl_oidc_client_secret_iedon";
+    "kioubit.auth" = "drasl_oidc_client_secret_kioubit";
+  };
 in
 {
-  sops.secrets.drasl_oidc_client_secret = { };
+  sops.secrets.drasl_oidc_client_secret_iedon = { };
+  sops.secrets.drasl_oidc_client_secret_kioubit = { };
 
   services.drasl = {
     enable = true;
     settings = draslToml // {
       RegistrationOIDC = map (provider: provider // {
-        ClientSecretFile = config.sops.secrets.drasl_oidc_client_secret.path;
+        ClientSecretFile = config.sops.secrets.${oidcClientSecretNames.${provider.Name}}.path;
       }) draslToml.RegistrationOIDC;
     };
   };
